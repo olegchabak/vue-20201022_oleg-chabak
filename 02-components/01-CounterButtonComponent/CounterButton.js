f99dc7dc5b8b6b4c45b0cf840f40ae3fca8edc29
olegchabak <chabak.o@brinex.ru>
2020-11-18 22:27:35 +0300

    02-components > 01-CounterButtonComponent - is done

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,10 +1,25 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" @click="increaseCount">{{ count }}</button>',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
 
   // Компонент должен иметь модель
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    increaseCount() {
+      this.$emit('increment', this.count + 1);
+    },
+  },
 };
